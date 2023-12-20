7d960ce4dad7fc88a8dc788ae3ab8eb24e1acab9
izancluac <153533514+izancluac@users.noreply.github.com>
2023-12-20 15:59:59 +0100

    Update Principal.java
    Tercere update

--- proyectoprueba/src/proyectoprueba/Principal.java
+++ proyectoprueba/src/proyectoprueba/Principal.java
@@ -6,7 +6,7 @@ public class Principal {
 		// TODO Auto-generated method stub
 		System.out.print("Holi wankenobi");
 		System.out.print("Agur yogur!");
-		
+		System.out.print("Ciao peacao");
 		}
 
 }
