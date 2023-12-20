532965f13d6b76900b12a047e404ffcdc811e55d
izanlopez <izanlopez@client89>
2023-12-20 15:16:51 +0100

    Version inicial

---
+++ proyectoprueba/src/proyectoprueba/Principal.java
@@ -0,0 +1,11 @@
+package proyectoprueba;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Holi wankenobi");
+		
+		}
+
+}
